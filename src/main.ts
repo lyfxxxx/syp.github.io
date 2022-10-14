46c5698e94ccab2cebf73c4d69cc71e3b9c0a662
suyunpeng <894624801@qq.com>
2022-10-14 23:01:43 +0800

    fix: 修复iconfont.js引入错误

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,11 @@
 import { createApp } from 'vue';
 import Icon from './components/Icon.vue';
-import './styles/global.scss';
-import './styles/media.scss';
 import App from './App.vue'
 import router from './router';
+
+import './assets/iconfont.js';
+import './styles/global.scss';
+import './styles/media.scss';
 
 const app = createApp(App);
 app.use(router);
